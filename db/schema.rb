499c1c2eda3bd8fba8153712fe139a0f31c55d21
jimmy <chioulf2@yahoo.com.tw>
2015-02-22 21:27:31 +0800

    commmit

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150209164937) do
+ActiveRecord::Schema.define(version: 20150222110412) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
